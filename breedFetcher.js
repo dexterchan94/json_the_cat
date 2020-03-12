8686f99e698d31ea7766597bcf1bf838b9cd972d
Dexter Chan <dexterchann@gmail.com>
2020-03-12 01:01:58 +0000

    Refactored and added mocha and chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 const request = require("request");
 
-const query = process.argv[2];
-
-const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + query;
-
-request(URL, (error, response, body) => {
-  console.log("statusCode: ", response && response.statusCode);
-  if (error) {
-    console.log("Error: ", error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log("No results found!");
+const fetchBreedDescription = (breedName, callback) => {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  request(URL, (error, response, body) => {
+    // console.log("statusCode: ", response && response.statusCode);
+    if (error) {
+      callback(error);
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(null, "No results found!");
+      } else {
+        callback(null, data[0].description);
+      }
     }
-  }
-});
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
